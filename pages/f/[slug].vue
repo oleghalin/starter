d95742f6abd52cf515579044c108d024854e8424
Oleg Khalin <oleghalinua@gmail.com>
2023-02-21 22:56:54 +0000

    Update 6 files

---
+++ pages/f/[slug].vue
@@ -0,0 +1,15 @@
+<template>
+  <form action="/f/123" method="POST">
+    <button>submit</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { readMultipartFormData, getMethod } from "h3";
+const { data, error } = await useFetch("/api/products");
+const event = useRequestEvent();
+
+console.log(getMethod(event));
+
+console.log(data.value);
+</script>
